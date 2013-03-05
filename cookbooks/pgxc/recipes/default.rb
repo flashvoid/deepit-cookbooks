039a6f08135f4fdc28c4bc2f7a750558d73fbc01
Stas A. Kraev <stanislav.kraev@clearscale.net>
2013-03-05 15:12:38 +0000

    gitignore for .swp

--- cookbooks/pgxc/recipes/default.rb
+++ cookbooks/pgxc/recipes/default.rb
@@ -2,7 +2,15 @@
 # Cookbook Name:: pgxc
 # Recipe:: default
 #
-# Copyright 2013, YOUR_COMPANY_NAME
+# Copyright 2013, deepIT
 #
 # All rights reserved - Do Not Redistribute
 #
+
+remote_file "/tmp/#{node["pgxc"]["package"]}" do
+	source "#{node["pgxc"]["url"]}/#{node["pgxc"]["package"]}"
+end
+
+package "pgxc" do
+	source "/tmp/#{node["pgxc"]["package"]}"
+end
